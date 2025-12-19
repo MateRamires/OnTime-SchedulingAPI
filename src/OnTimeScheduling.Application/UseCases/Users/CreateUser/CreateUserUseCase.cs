bb266145cec09bc91e5e3c3b94f053c7123397be
MateRamires <matheushenriqueramires@gmail.com>
2025-12-19 17:52:50 -0300

    Create Create User Use case interface and class

---
+++ src/OnTimeScheduling.Application/UseCases/Users/CreateUser/CreateUserUseCase.cs
@@ -0,0 +1,11 @@
+﻿using OnTimeScheduling.Domain.Entities.User;
+
+namespace OnTimeScheduling.Application.UseCases.Users.CreateUser;
+
+public class CreateUserUseCase : ICreateUserUseCase
+{
+    public Task<Guid> ExecuteAsync(User user)
+    {
+        
+    }
+}
---
+++ src/OnTimeScheduling.Application/UseCases/Users/CreateUser/ICreateUserUseCase.cs
@@ -0,0 +1,8 @@
+﻿using OnTimeScheduling.Domain.Entities.User;
+
+namespace OnTimeScheduling.Application.UseCases.Users.CreateUser;
+
+public interface ICreateUserUseCase
+{
+    public Task<Guid> ExecuteAsync(User user);
+}
