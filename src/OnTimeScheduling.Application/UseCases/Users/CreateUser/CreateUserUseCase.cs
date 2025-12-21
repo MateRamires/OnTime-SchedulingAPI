5b52c6859da7e1b735e1ddb2f5962c82fa130115
MateRamires <matheushenriqueramires@gmail.com>
2025-12-21 19:02:02 -0300

    Configuring Dependency Injenction in infrastructure & Application

--- src/OnTimeScheduling.Application/UseCases/Users/CreateUser/CreateUserUseCase.cs
+++ src/OnTimeScheduling.Application/UseCases/Users/CreateUser/CreateUserUseCase.cs
@@ -1,11 +1,26 @@
-﻿using OnTimeScheduling.Domain.Entities.User;
+﻿using OnTimeScheduling.Application.Repositories.UnitOfWork;
+using OnTimeScheduling.Application.Repositories.Users;
+using OnTimeScheduling.Domain.Entities.User;
 
 namespace OnTimeScheduling.Application.UseCases.Users.CreateUser;
 
 public class CreateUserUseCase : ICreateUserUseCase
 {
-    public Task<Guid> ExecuteAsync(User user)
+    private readonly IUserRepository _userRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    public CreateUserUseCase(IUserRepository userRepository, IUnitOfWork unitOfWork)
     {
+        _userRepository = userRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Guid> ExecuteAsync(User user)
+    {
+        var newUser = new User();
+
+        await _userRepository.AddAsync(newUser);
+        await _unitOfWork.CommitAsync();
         
+        return user.Id;
     }
 }
---
+++ src/OnTimeScheduling.Infrastructure/DependencyInjenctionExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnTimeScheduling.Application.Repositories.UnitOfWork;
+using OnTimeScheduling.Application.Repositories.Users;
+using OnTimeScheduling.Infrastructure.Persistence.DataAccess;
+
+namespace OnTimeScheduling.Infrastructure;
+
+public static class DependencyInjenctionExtension
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration) 
+    {
+        services.AddDbContext<AppDbContext>(opt =>
+            opt.UseNpgsql(configuration.GetConnectionString("PostgreSQL"))
+        );
+
+        services.AddScoped<IUserRepository, IUserRepository>();
+
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<AppDbContext>());
+
+    }
+}
