69bcf1b34e9ffc83d79bd43a1a630df8c193c58a
MateRamires <matheushenriqueramires@gmail.com>
2026-01-07 19:48:15 -0300

    Added a new TODO comment to the controller

--- src/Backend/OnTimeScheduling.Api/Controllers/UserController.cs
+++ src/Backend/OnTimeScheduling.Api/Controllers/UserController.cs
@@ -4,10 +4,12 @@ using OnTimeScheduling.Communication.Requests;
 
 namespace OnTimeScheduling.Api.Controllers
 {
+    
     [ApiController]
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
+        //TODO: Add the expected response or the expected error to every endpoint
         [HttpPost]
         public async Task<IActionResult> Register([FromServices] ICreateUserUseCase useCase, [FromBody] RequestRegisterUserJson request, CancellationToken ct) 
         {
