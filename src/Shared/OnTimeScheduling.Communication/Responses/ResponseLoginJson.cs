dfb5d37dc00256456a95ee49e4ffaf7a28b99606
MateRamires <matheushenriqueramires@gmail.com>
2026-01-15 20:24:54 -0300

    Changing Reponse Login's variable name

--- src/Shared/OnTimeScheduling.Communication/Responses/ResponseLoginJson.cs
+++ src/Shared/OnTimeScheduling.Communication/Responses/ResponseLoginJson.cs
@@ -3,5 +3,5 @@
 public class ResponseLoginJson
 {
     public string Name { get; set; } = string.Empty;
-    public string Token { get; set; } = string.Empty;
+    public string AccessToken { get; set; } = string.Empty;
 }
