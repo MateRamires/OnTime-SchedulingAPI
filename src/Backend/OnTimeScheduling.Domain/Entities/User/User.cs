906fa318efac3912dfc3730d3a99e9172e40891f
MateRamires <matheushenriqueramires@gmail.com>
2026-02-03 20:18:51 -0300

    Creating the Company entity and fixing small warning on user entity

--- src/Backend/OnTimeScheduling.Domain/Entities/User/User.cs
+++ src/Backend/OnTimeScheduling.Domain/Entities/User/User.cs
@@ -8,9 +8,9 @@ public class User : BaseEntity
 {
     public Guid? CompanyId { get; private set; }
 
-    public string Name { get; private set; } = null;
-    public string Email { get; private set; } = null;
-    public string PasswordHash { get; private set; } = null;
+    public string Name { get; private set; } = null!;
+    public string Email { get; private set; } = null!;
+    public string PasswordHash { get; private set; } = null!;
 
     public UserRole Role { get; private set; }
     public RecordStatus Status { get; private set; }
