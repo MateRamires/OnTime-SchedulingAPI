31d9346a10fda5f1ccc9b00ea4fcf4d2e3177faa
MateRamires <matheushenriqueramires@gmail.com>
2026-02-22 16:38:16 -0300

    Service table migration added

--- src/Backend/OnTimeScheduling.Infrastructure/Persistence/Migrations/20260222192624_AddServiceTable.Designer.cs
+++ src/Backend/OnTimeScheduling.Infrastructure/Persistence/Migrations/20260222192624_AddServiceTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using OnTimeScheduling.Infrastructure.Persistence.DataAccess;
@@ -11,9 +12,11 @@ using OnTimeScheduling.Infrastructure.Persistence.DataAccess;
 namespace OnTimeScheduling.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260222192624_AddServiceTable")]
+    partial class AddServiceTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
