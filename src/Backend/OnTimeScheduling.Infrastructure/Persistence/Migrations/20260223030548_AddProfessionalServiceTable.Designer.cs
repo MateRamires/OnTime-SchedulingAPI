eb34d6f5b5ba2e4e61f894745e5da939416a41fe
MateRamires <matheushenriqueramires@gmail.com>
2026-02-23 00:06:12 -0300

    Creating the Professional Service Migration

--- src/Backend/OnTimeScheduling.Infrastructure/Persistence/Migrations/20260223030548_AddProfessionalServiceTable.Designer.cs
+++ src/Backend/OnTimeScheduling.Infrastructure/Persistence/Migrations/20260223030548_AddProfessionalServiceTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using OnTimeScheduling.Infrastructure.Persistence.DataAccess;
@@ -11,9 +12,11 @@ using OnTimeScheduling.Infrastructure.Persistence.DataAccess;
 namespace OnTimeScheduling.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260223030548_AddProfessionalServiceTable")]
+    partial class AddProfessionalServiceTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
