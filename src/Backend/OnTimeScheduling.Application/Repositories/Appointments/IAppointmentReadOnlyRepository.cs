a68fc5d93dc50b94547323ae04060a10b7513908
MateRamires <matheushenriqueramires@gmail.com>
2026-03-06 03:22:54 -0300

    Finished Appointment Repository

---
+++ src/Backend/OnTimeScheduling.Application/Repositories/Appointments/IAppointmentReadOnlyRepository.cs
@@ -0,0 +1,17 @@
+﻿using OnTimeScheduling.Domain.Entities.Appointments;
+
+namespace OnTimeScheduling.Application.Repositories.Appointments;
+
+public interface IAppointmentReadOnlyRepository
+{
+    Task<bool> HasOverlappingAppointment(
+        Guid professionalId,
+        DateTime startTime,
+        DateTime endTime,
+        CancellationToken ct = default);
+    Task<List<Appointment>> GetAppointmentsByPeriod(
+        Guid professionalId,
+        DateTime startPeriod,
+        DateTime endPeriod,
+        CancellationToken ct = default);
+}
